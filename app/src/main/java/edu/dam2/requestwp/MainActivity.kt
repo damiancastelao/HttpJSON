51e9537ca490252949a86f11dce3cba357f73b4e
Damian Nogueiras <damiannogueiras@gmail.com>
2019-01-24 19:21:24 +0100

    doAsync para la petición

---
+++ app/src/main/java/edu/dam2/requestwp/MainActivity.kt
@@ -0,0 +1,42 @@
+package edu.dam2.requestwp
+
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.*
+import java.net.URL
+
+class MainActivity : Activity() {
+
+    // para la etiqueta de los logs
+    val LOGTAG = "requestwp"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Cuando le damos al boton empezamos una corutina
+        button.setOnClickListener { peticionwp() }
+
+    }
+
+    /**
+     * Hace una peticion a Wordpress. Utiliza API Request
+     */
+    fun peticionwp() {
+        var respuesta = ""
+        // lanza la corutina NO en el hilo principal
+        doAsync {
+            // peticion a wordpress
+            respuesta = URL("http://34.242.231.110/wp5/?rest_route=/wp/v2/posts/1").readText()
+            // Accedemos al hilo principal
+            uiThread {
+                response.text = respuesta
+                Log.d(LOGTAG, respuesta)
+                longToast("Request performed")
+            }
+        }
+    }
+
+}
